823730b54cfeb0460532d3b1941d799237040b50
Charles <cchester1@angelo.edu>
2023-01-31 15:22:44 -0600

    storage logic

--- dom.js
+++ dom.js
@@ -1,14 +1,26 @@
 const masterDiv = $('#masterDiv')
 
 function storeFirstVal (firstVal) { localStorage.setItem('firstVal', JSON.stringify(firstVal)) }
+function storeSecondVal (secondVal) { localStorage.setItem('secondVal', JSON.stringify(secondVal)) }
+function storeArithmetic (symbol) { localStorage.setItem('symbol', JSON.stringify(symbol))}
+function clearLocalStorage () { localStorage.clear() }
 let store = []
 
 $(masterDiv).on('click', function (event) {
       let el = event.target
       let elVal = $(el).text()
       
-      if(elVal === '+' || elVal === '-' || elVal === '*' || elVal === '=') {
-            let firstVal = store.join('')
-            storeFirstVal(firstVal)
+      if(elVal === '+' || elVal === '-' || elVal === '*') {
+            let symbol = elVal
+            let saveVal = store.join('')
+
+            storeArithmetic(symbol)
+            storeFirstVal(saveVal)
+
+            store = [];
+      } else if(elVal === 'clear') {
+            clearLocalStorage()
+      } else {
+            store.push(elVal)
       }
 })
