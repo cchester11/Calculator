5d1ffd54de2a0b7f3f632faf3b508554a9eac1cd
Charles <cchester1@angelo.edu>
2023-01-31 16:47:56 -0600

    subtraction works

--- dom.js
+++ dom.js
@@ -19,22 +19,34 @@ $(masterDiv).on('click', function (event) {
       
       // the following are our conditional statements
       if(elVal === '+' || elVal === '-' || elVal === '*') {
+            // variables of the conditional scope
             let symbol = elVal
             let saveVal = store.join('')
             let checkVal = JSON.parse(localStorage.getItem('firstVal'))
 
+            // if the localStorage variable 'firstVal' already has a stored value, then send the user an error
+            // if the 'firstVal' is empty, store the first value using the storeFirstVal() utility above
             if(checkVal) { 
-                  window.alert('error; first value already saved') 
+                  window.alert('error; first value already saved. Either click the equal sign or clear to start over') 
             } else { 
                   storeFirstVal(saveVal) 
             }
 
+            // capture the arithmetic symbol clicked and store is under the key 'symbol'
             storeArithmetic(symbol)
 
+            // empty the store array
             store = [];
+            
+            // if the user clicks 'clear' empty all storage variables
       } else if(elVal === 'clear') {
             clearLocalStorage()
+            answer_element.text('')
+
+            // if the user clicks the equal sign
       } else if(elVal === '=') {
+            // save the store array as one value
+            // store the value of the store array in the localStorage variable 'secondValue'
             let saveVal = store.join('')
             storeSecondVal(saveVal)
 
@@ -46,7 +58,7 @@ $(masterDiv).on('click', function (event) {
             // conditional statments for what equation to perform
             if(symbol === '-') {
                   let x = firstVal - secondVal
-                  answer_element.textContent(x)
+                  $(answer_element).text(x)
             }
       } else {
             store.push(elVal)
